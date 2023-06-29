070ba174c0a950f624de866d2c16932cd2e50542
alexeirybak <rybak2108@yandex.ru>
2023-06-30 00:03:01 +0300

    I followed your recommendation

--- script.js
+++ script.js
@@ -1,69 +1,54 @@
 "use strict";
 
-  const levels = [
-    {
-      level: 1,
-    },
-
-    {
-      level: 2,
-    },
-
-    {
-      level: 3,
-    },
-  ];
+const levels = [ { level: 1, }, { level: 2, }, { level: 3, }, ];
 
   let currentSelectedLevel = null;
-  const listLevels = document.getElementById("levels");
-  const buttonStart = document.querySelector(".button-start")
+  const listLevels = document.getElementById('levels');
+  const buttonStart = document.querySelector('.button-start')
+  const form = document.querySelector('.form-block');
 
 // ---------- Рендерим уровни -------------------------------------------------
   const renderLevels = () => {
     const levelsHtml = levels.map((level) => {
-        return `<div class="level-item">${level.level}</div>`;
-      }).join('');
+        return `<label class="level">
+        <input type="radio" name="level" value="${level.level}">${level.level}</label>`;
+  }).join('');
       listLevels.innerHTML = levelsHtml;
     }
 
   renderLevels()
 
 // ---------- Выбираем уровень ------------------------------------------------
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+radioButtons.forEach((radioButton) => {
+  radioButton.addEventListener("change", () => {
+    radioButtons.forEach((btn) => {
+      if (btn !== radioButton) {
+        btn.parentElement.classList.remove('chosen-level');
+      }
+    });
+    radioButton.parentElement.classList.add('chosen-level');
+    currentSelectedLevel = radioButton;
+  });
+});
 
+// ---------- Проверяем, выбран ли уровень ------------------------------------
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const checkedLevel = document.querySelector('input[type="radio"]:checked');
+  if (checkedLevel) {
+    currentSelectedLevel = checkedLevel.value;
+    startGame();
+  } else {
+    alert("Выберите уровень");
+  }
+});
 
-function choseLevel() { 
-    const offerLevels = document.querySelectorAll(".level-item"); 
-        offerLevels.forEach((offerLevel) => { 
-        offerLevel.addEventListener('click', (event) => { 
-            event.stopPropagation(); 
-
-            if (currentSelectedLevel !== null) { 
-                currentSelectedLevel.classList.remove("chosen-level"); 
-            } 
-
-        offerLevel.classList.add("chosen-level"); 
-        currentSelectedLevel = offerLevel; 
-        }) 
-    }) 
-}
-
-choseLevel()
-
+// ---------- Запускаем новую страницу ----------------------------------------
 function startGame() { 
-    buttonStart.addEventListener('click', (event) => { 
-        event.stopPropagation(); 
-
-        if (currentSelectedLevel === null) {
-            alert("Выберите уровень");
-            return;
-        }
-
-        const newPage = document.createElement('div');
-        newPage.classList.add('yourLevel');
-        const selectedLevelNumber = currentSelectedLevel.textContent;
-        newPage.textContent = `Вы выбрали уровень ${selectedLevelNumber}`;
-        document.body.innerHTML = '';
-        document.body.appendChild(newPage);
-    }) 
+  const newPage = document.createElement('div');
+  newPage.classList.add('yourLevel');
+  newPage.textContent = `Вы выбрали уровень ${currentSelectedLevel}`;
+  document.body.innerHTML = '';
+  document.body.appendChild(newPage);
 }
-startGame();
