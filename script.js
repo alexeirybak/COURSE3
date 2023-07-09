67c4d1e4ab53efa95e52829936f6f3d304824281
alexeirybak <rybak2108@yandex.ru>
2023-07-09 10:40:19 +0300

    Changes 1

--- script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import { renderStartScreen } from "./screen-start.js";
-import { renderCards } from "./screen-card.js";
-
-renderStartScreen();
-renderCards();
-
---
+++ src/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import { getScreen } from './screen-start.js';
+
+getScreen();
