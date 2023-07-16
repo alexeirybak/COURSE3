8bac71a0b99cb807975fd0a5d905e35131c7242f
Alexei Rybak <rybak2108@yandex.ru>
2023-07-16 11:58:52 +0300

    Dostalo

--- src/screen-start.ts
+++ src/screen-start.ts
@@ -1,59 +1,79 @@
 import { renderCards } from './screen-card';
 
+// ---------- Рендерим первую страницу ----------------------------------------
 export let screenFirstElement: HTMLElement | null = document.querySelector('.front');
 
-export const screenStart: string = `<form class="form-block">
-                                      <p class="level-choice">Выбери сложность</p>
-                                      <div class="level" id="levels"></div>
-                                      <button type="submit" class="button-start">Старт</button>
-                                    </form>`;
+const screenStart = `<form class="form-block">
+                            <p class="level-choice">Выбери сложность</p>
+                                <div class="level" id="levels"></div>
+                                <button type="submit" class="button-start">Старт</button>
+                    </form>`;
 
 if (screenFirstElement) {
   screenFirstElement.innerHTML = screenStart;
 }
 
-const levels: { level: number }[] = [{ level: 1 }, { level: 2 }, { level: 3 }];
+const levels = [{ level: 1 }, { level: 2 }, { level: 3 }];
 
 export let currentSelectedLevel: number | null = null;
-const listLevels: HTMLElement | null = document.getElementById('levels');
-const form: HTMLFormElement | null = document.querySelector('.form-block');
+const listLevels = document.getElementById('levels');
+const form = document.querySelector('.form-block');
 
-export const getScreen = (): void => {
+export const getScreen = () => {
   if (screenFirstElement) {
     screenFirstElement.classList.add('front');
   }
+  // ---------- Рендерим уровни -------------------------------------------------
+  const renderLevels = () => {
+    const levelsHtml = levels
+      .map((level) => {
+        return `<label class="level">
+                        <input type="radio" name="level" value="${level.level}">${level.level}</label>`;
+      })
+      .join('');
+      if (listLevels) {
+        listLevels.innerHTML = levelsHtml;
+      }
+  };
+  renderLevels();
+
+  // ---------- Выбираем уровень ------------------------------------------------
+  const radioButtons = document.querySelectorAll('input[type="radio"]') as NodeListOf<HTMLInputElement>;
+  
+  radioButtons.forEach((radioButton) => {
+    radioButton.addEventListener('change', () => {
+      radioButtons.forEach((btn) => {
+        if (btn !== radioButton) {
+          if (btn.parentElement) {
+            btn.parentElement.classList.remove('chosen-level');
+          }
+        }
+      });
+  
+      if (radioButton.parentElement) {
+        radioButton.parentElement.classList.add('chosen-level');
+        currentSelectedLevel = parseInt(radioButton.value);
+      }
+    });
+  });
 
   if (form) {
-    const radioButtons: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[type="radio"]');
-
-    radioButtons.forEach((radioButton) => {
-      radioButton.addEventListener('change', () => {
-        radioButtons.forEach((btn) => {
-          if (btn !== radioButton) {
-            btn.parentElement?.classList.remove('chosen-level');
-          }
-        });
-
-        radioButton.parentElement?.classList.add('chosen-level');
-        currentSelectedLevel = radioButton.value ? parseInt(radioButton.value) : null;
-      });
-    });
-
     form.addEventListener('submit', (event) => {
       event.preventDefault();
-
-      const checkedLevel: string | null = (form.elements.namedItem('level') as HTMLInputElement)?.value;
-
-      if (checkedLevel) {
-        currentSelectedLevel = parseInt(checkedLevel);
-
-        if (screenFirstElement) {
-          screenFirstElement.style.display = 'none';
+      const target = event.target as HTMLFormElement;
+      if (target) {
+        if ('level' in target.elements) {
+          const checkedLevel = (target.elements['level'] as HTMLInputElement).value;
+          if (checkedLevel) {
+            currentSelectedLevel = parseInt(checkedLevel);
+            if (screenFirstElement) {
+              screenFirstElement.style.display = 'none';
+            }
+            renderCards();
+          } else {
+            alert('Выберите уровень');
+          }
         }
-
-        renderCards();
-      } else {
-        alert('Выберите уровень');
       }
     });
   }
